782ffff9f0f808290322d1b44cdf9c363b515447
pravinmane08 <thepravinmane08@gmail.com>
2026-03-19 16:38:37 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+
+        requirements = [req.strip() for req in requirements]
+
+        requirements = [
+            req for req in requirements
+            if req and req != HYPEN_E_DOT
+        ]
+
+    return requirements
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Pravin',
+    author_email='thepravinmane08@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
