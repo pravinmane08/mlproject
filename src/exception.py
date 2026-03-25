7dd416f9d8cd006fd6953c4934c6c161acb52f7e
pravinmane08 <thepravinmane08@gmail.com>
2026-03-25 21:20:57 +0530

    adding lib

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+from src.logger import logging
+
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = sys.exc_info()
@@ -19,3 +21,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
