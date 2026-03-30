af7eb2ed5e5ed11ad5fc148f3c0a636e33b816e3
pravinmane08 <thepravinmane08@gmail.com>
2026-03-30 20:07:23 +0530

    Hyperparameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,7 +18,7 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object
-from src.utils import evaluate_models  
+from src.utils import evaluate_models   
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path: str = os.path.join("artifacts", "model.pkl")
@@ -49,14 +49,48 @@ class ModelTrainer:
                 "AdaBoost": AdaBoostRegressor(),
             }
 
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
            
-            model_report = evaluate_models(
-                X_train=x_train,
-                y_train=y_train,
-                X_test=x_test,
-                y_test=y_test,
-                models=models
-            )
+            
+            model_report:dict=evaluate_models(X_train=x_train,y_train=y_train,X_test=x_test,y_test=y_test,
+                                             models=models,param=params)
 
             best_model_score = max(model_report.values())
 
--- src/utils.py
+++ src/utils.py
@@ -4,15 +4,14 @@ import dill
 
 import numpy as np
 import pandas as pd
+import pickle
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
-        import os
-        import dill
-
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
 
@@ -23,14 +22,23 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
   
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
 
+
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            # model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
 
